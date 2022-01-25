356c0236bba6c60ea932a00aac9b3efbb7785d96
supriyanto4id <supriprosesor@gmail.com>
2022-01-25 13:09:31 +0800

    add address

--- src/data/Address.kt
+++ src/data/Address.kt
@@ -3,5 +3,15 @@ package data
 class Address {
     var street :String=""
     var city :String= ""
-    var contry:String=""
+    var contry:String=" Indonesia"
+
+
+    constructor(paramStreet:String, paramCity:String){
+        street = paramStreet
+        city = paramCity
+    }
+    constructor(paramStreet: String)
+            : this(paramStreet){
+        street =paramStreet
+    }
 }
